feb3b73fc93cd06bde667a1bd1b4a647c444a822
Martijn Nieuwland <martijn@nieuwland.mn>
2023-04-28 10:11:05 +0200

    edit session 3 and 4

--- 0414_B2_session3/0414_B2_session3.py
+++ 0414_B2_session3/0414_B2_session3.py
@@ -1,23 +1,49 @@
-f = open("Sessions/0414_B2_session3/mbox-short.txt", "r")
-text = f.readlines()
-total = 0
-emails = {}
+# f = open("Sessions/0414_B2_session3/mbox-short.txt", "r")
+# text = f.readlines()
+# total = 0
+# emails = {}
 
-for line in text:
-    if line.startswith("From:"):
-        total += 1
-        email = (line.strip("\n")).split(" ")[1]
-        if email in emails:
-            emails[email] += 1
-        else:
-            emails[email] = 1
+# for line in text:
+#     if line.startswith("From:"):
+#         total += 1
+#         email = (line.strip("\n")).split(" ")[1]
+#         if email in emails:
+#             emails[email] += 1
+#         else:
+#             emails[email] = 1
                     
-for email, count in emails.items():
-    print(f"{email}: {count}")
+# for email, count in emails.items():
+#     print(f"{email}: {count}")
 
-for email, messages in emails.items():
-    if messages == max(emails.values()):
-        sender = email
+# for email, messages in emails.items():
+#     if messages == max(emails.values()):
+#         sender = email
 
-print(f"\nTotal number of emails: {total}")
-print(f"\nMost emails sent by: {sender} ({max(emails.values())})\n")
+# print(f"\nTotal number of emails: {total}")
+# print(f"\nMost emails sent by: {sender} ({max(emails.values())})\n")
+
+
+# 2. Write a list comprehension to generate a list of all possible pairs of numbers from two given lists.
+
+# x = [1, 2, 3]
+# y = [4, 5, 6]
+
+# xy = [i * n for i in x for n in y]
+
+# print(xy)
+
+
+# 3. Write a list comprehension to find all the prime numbers between 1 and 100.
+
+primes = [prime for prime in range(2, 101) if all(prime % i != 0 for i in range(2, prime))]
+
+# for p in range(1, 101):
+#     primes.append(p)
+#     for i in range(2, p):
+#         if p % i == 0:
+#             primes.remove(p)
+#             break
+
+            
+print(primes)
+
